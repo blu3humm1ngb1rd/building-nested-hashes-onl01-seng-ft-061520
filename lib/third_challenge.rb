481faf5b1c37824aaf95833fb80591599adf8689
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-21 00:36:36 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,13 +12,13 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+       
         },
       :matriarch => {
-        #your key/value pairs here
+        
         },
       :heroine => {
-        #your key/value pairs here
+
         },
       :heroine_friends => []
    }
